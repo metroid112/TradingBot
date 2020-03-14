c95742587e9a47c4508038618910064fbc370025
metroid112 <federico.aguilera112@gmail.com>
2020-03-14 19:26:39 -0300

    more stocks

--- Quantopian/SuperAlgorithm.py
+++ Quantopian/SuperAlgorithm.py
@@ -1,5 +1,7 @@
 def initialize(context):
     context.stocks = [sid(24), sid(46632), sid(16841), sid(39840), sid(19725), sid(5061), sid(351)]
+    context.stocks_weight = 1.0 / len(context.stocks)
+
     schedule_function(trading, 
                       date_rules.every_day(), 
                       time_rules.market_open(hours=1))
@@ -7,15 +9,15 @@ def initialize(context):
     
 def trading(context, data):
     for stock in context.stocks:
-	    hist = data.history(stock, 'price', 50, '1d')
-	    
-	    sma_50 = hist.mean()
-	    sma_20 = hist[-20:].mean()
-	    
-	    open_orders = get_open_orders()
-	    
-	    if stock not in open_orders:
-	        if sma_20 > sma_50:
-	            order_target_percent(stock, 1.0)
-	        elif sma_20 < sma_50:
-	            order_target_percent(stock, -1.0)
+        hist = data.history(stock, 'price', 50, '1d')
+
+        sma_50 = hist.mean()
+        sma_20 = hist[-20:].mean()
+        
+        open_orders = get_open_orders()
+        
+        if stock not in open_orders and data.can_trade(stock):
+            if sma_20 > sma_50:
+                order_target_percent(stock, context.stocks_weight)
+            elif sma_20 < sma_50:
+                order_target_percent(stock, -context.stocks_weight)
